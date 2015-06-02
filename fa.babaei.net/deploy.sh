ef321f037af2ab48c55060bfe09dddef26fbaf3e
Mohammad S. Babaei <info@babaei.net>
2015-06-02 15:05:00 +0430

    permission fix

--- fa.babaei.net/deploy.sh
+++ fa.babaei.net/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -rf public/*
-hexo generate && rsync -avze 'ssh -p 1317' --perms --chmod=a+rwx,g+rwx,o-wx --delete public/ wrs@199.48.133.134:~/babaei.net/wfa
+hexo generate && rsync -avze 'ssh -p 1317' --perms --chmod=u+rwx,g+rx,o+rx,g-w,o-wx --delete public/ wrs@199.48.133.134:~/babaei.net/wfa
