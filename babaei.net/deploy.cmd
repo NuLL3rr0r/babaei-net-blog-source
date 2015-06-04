9e1ac9ac9c580b38856f2a78b5d577c2e82b83ea
Mohammad S. Babaei <info@babaei.net>
2015-06-04 23:54:32 +0430

    Deploy for windows

---
+++ babaei.net/deploy.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+cd %~dp0
+
+set LANG=en_US.UTF-8
+set MM_CHARSET=UTF-8
+set LC_ALL=en_US.UTF-8
+chcp 65001
+
+set PATH=%~dp0..\cwRsync;%~dp0..\Git\bin;%PATH%
+
+echo %PATH% & echo. & echo %LANG% & echo %MM_CHARSET% & echo %LC_ALL% & echo.
+
+echo Removing public dir...
+rmdir /S /Q public
+
+echo.
+
+echo Starting Hexo...
+hexo generate && rsync -avze 'ssh -p 1317' --perms --chmod=u+rwx,g+rx,o+rx,g-w,o-wx --delete public/ wrs@199.48.133.134:~/babaei.net/wen
+
+pause
+
