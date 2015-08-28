a48d6caca11038c462ab0702170f23c44373469c
Mohammad S. Babaei <info@babaei.net>
2015-08-28 21:09:56 +0430

    New dev layouts

--- hexo-cli.cmd
+++ hexo-cli.cmd
@@ -7,7 +7,7 @@ set MM_CHARSET=UTF-8
 set LC_ALL=en_US.UTF-8
 chcp 65001
 
-set PATH=%~dp0cwRsync;%~dp0Git\bin;%PATH%
+set PATH=%~dp0..\_tools\cwRsync;%~dp0..\_tools\Git\bin;%PATH%
 
 echo %PATH% & echo. & echo %LANG% & echo %MM_CHARSET% & echo %LC_ALL% & echo.
 
