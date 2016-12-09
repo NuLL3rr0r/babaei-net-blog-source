841e70a3119f09f840a7a5e8f5de137cfefafddb
Mohammad S. Babaei <info@babaei.net>
2016-12-09 15:34:46 +0330

    add hugo-cli.cmd script for windows

---
+++ hugo-cli.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+cd %~dp0
+
+set LANG=en_US.UTF-8
+set MM_CHARSET=UTF-8
+set LC_ALL=en_US.UTF-8
+
+set GOROOT=E:\OPT\Go\
+
+set PATH=E:\OPT\Python27\Scripts;%PATH%
+set PATH=E:\OPT\Python27;%PATH%
+set PATH=E:\OPT\cwRsync;%PATH%
+set PATH=E:\OPT\Git\bin;%PATH%
+set PATH=E:\OPT\Hugo;%PATH%
+
+echo.
+echo %LANG%
+echo %MM_CHARSET%
+echo %LC_ALL%
+echo.
+echo %PATH%
+echo.
+
+%comspec% /k
