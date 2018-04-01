dfb972ac3fc92f06ed3e0c4a9d1dd0c8c056ed64
Mohammad S. Babaei <info@babaei.net>
2018-04-01 15:57:23 +0430

    add git revision detection and inclusion in the footer

--- deploy-fa.babaei.net.sh
+++ deploy-fa.babaei.net.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ./fa.babaei.net/ \
     && rm -rf public \
-    && hugo build \
+    && GIT_COMMIT_SHA=`git rev-parse --verify HEAD` GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD` \
+    hugo build \
     && rsync -avze 'ssh -p 1317' --perms --chmod=u+rwx,g+rx,o+rx,g-w,o-wx --delete public/ wrs@199.48.133.134:~/babaei.net/wfa
