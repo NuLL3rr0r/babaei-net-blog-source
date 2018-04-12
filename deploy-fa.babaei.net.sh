c3f6b9d25497f9a12d597bd819ff71252193f4ef
Mohammad S. Babaei <info@babaei.net>
2018-04-12 19:11:05 +0430

    fix build script

--- deploy-fa.babaei.net.sh
+++ deploy-fa.babaei.net.sh
@@ -2,5 +2,5 @@
 cd ./fa.babaei.net/ \
     && rm -rf public \
     && GIT_COMMIT_SHA=`git rev-parse --verify HEAD` GIT_COMMIT_SHA_SHORT=`git rev-parse --short HEAD` \
-    hugo build \
+    hugo \
     && rsync -avze 'ssh -p 1317' --perms --chmod=u+rwx,g+rx,o+rx,g-w,o-wx --delete public/ wrs@199.48.133.134:~/babaei.net/wfa
